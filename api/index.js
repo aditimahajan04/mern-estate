d9c103d4000033a5fde87b34cabc0f9c100d6481
aditimahajan04 <mahajanaditi108@gmail.com>
2024-09-27 16:24:45 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -1,67 +1,31 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// import userRouter from './routes/user.route.js';
-// import authRouter from './routes/auth.route.js';
-// dotenv.config();
-
-// mongoose.connect(process.env.MONGO).then(()=>{
-//     console.log('Connected to MongoDB!');
-// }).catch((err)=>{
-//     console.log(err);
-// })
-
-// const app = express();
-// app.use(express.json());  
-
-// app.listen(3000,()=>{
-//     console.log('Server is running on port 3000')
-// });
-
-// app.use("/api/user",userRouter);
-// app.use('/api/auth',authRouter);
-
-
-// app.use((err,req,res,next)=>{
-//     const statusCode = err.statusCode || 500;
-//     const message = err.message || 'Internal Server Error';
-//     return res.status(statusCode).json({
-//         success: false,
-//         statusCode,
-//         message,
-//     });
-// });
-
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Import CORS
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDB!');
-}).catch((err) => {
+}).catch((err)=>{
     console.log(err);
-});
+})
 
 const app = express();
+app.use(express.json());  
 
-// Add the CORS middleware here
-app.use(cors()); // Enable CORS for all routes
+app.use(cookieParser());
 
-app.use(express.json());
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000')
 });
 
-app.use("/api/user", userRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/user",userRouter);
+app.use('/api/auth',authRouter);
 
-app.use((err, req, res, next) => {
+
+app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
@@ -70,3 +34,4 @@ app.use((err, req, res, next) => {
         message,
     });
 });
+
